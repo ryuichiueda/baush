1b1ff11e7420e6c25c29bde384fc7dcb73e3be6a
Ryuichi Ueda <ryuichiueda@gmail.com>
2021-01-31 17:21:42 +0900

    Add a prototype of token class

--- src/baush.cc
+++ src/baush.cc
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+class Element
+{
+public:
+	virtual bool parse(string &buffer) = 0;
+private:
+	vector<Element *> _elements; // element tree
+};
+
+class Token : public Element
+{
+public: 
+	virtual bool parse(string &buffer){
+		return true;
+	}
+};
 
 int main(int argc, char const* argv[])
 {
+	cout << "$ " << flush;
+	
+	string line;
+	cin >> line;
+
+	cout << line << endl;
+
 	return 0;
 }
